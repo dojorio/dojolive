482e27bed1c2eb8a73b7faed2e6cc26f3b7c83fc
Drumond <daniel.augusto@loadsmart.com>
2020-05-16 18:20:37 -0300

    Daniel + Rodolfo

--- 2020/20200516 - calculadora - python/calculadora_test.py
+++ 2020/20200516 - calculadora - python/calculadora_test.py
@@ -1,38 +1,51 @@
 import pytest
 
+
 def calcule(expressao):
-    if len(expressao) == 1:
+    if ' ' not in expressao.strip():
         return int(expressao)
 
     expressao = expressao.replace(" ", "")
-    operando1 = expressao[0]
-    operador = expressao[1]
-    operando2 = expressao[2]
 
-    if operador == '+':
+    operadores = {
+        '+': sum,
+    }
+    if '+' in expressao:
+        operando1, operando2 = expressao.split('+')
         return int(operando1) + int(operando2)
-    elif operador == '*':
+
+    if '*' in expressao:
+        operando1, operando2 = expressao.split('*')
         return int(operando1) * int(operando2)
-    elif operador == '-':
-        return int(operando1) - int(operando2)
+
+    operando1, operando2 = expressao.split('-')
+    return int(operando1) - int(operando2)
 
 
 @pytest.mark.parametrize('expressao,resultado', [
     # nenhum operador
     ("0", 0),
     ("4", 4),
+    ('10', 10),
+    ('100', 100),
+    ('100 ', 100),
+
     # um operador
     ('0 + 0', 0),
     ('0 + 1', 1),
     ('1 + 1', 0b10),
     ('1 + 2', 3),
+    ('10 + 2', 12),
+    ('10 + 20', 30),
 
     ('0 * 0', 0),
     ("1 * 1", 1),
     ("1 * 2", 2),
     ("5 * 2", 10),
+    ("50 * 2", 100),
 
     ("5 - 2", 3),
+    ("50 - 2", 48),
 ])
 def test_calcule(expressao, resultado):
     assert calcule(expressao) == resultado
