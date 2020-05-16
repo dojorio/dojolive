0f78b8c22e4bd258e2f509d1e56e12a12aac2034
berrondo <claudio.berrondo@gmail.com>
2020-05-16 18:09:31 -0300

    Berrondo + Daniel

--- 2020/20200516 - calculadora - python/calculadora_test.py
+++ 2020/20200516 - calculadora - python/calculadora_test.py
@@ -3,9 +3,18 @@ import pytest
 def calcule(expressao):
     if len(expressao) == 1:
         return int(expressao)
-    if '1' in expressao and '*' in expressao:
-        return int(expressao[-1])
-    return expressao.count('1')
+
+    expressao = expressao.replace(" ", "")
+    operando1 = expressao[0]
+    operador = expressao[1]
+    operando2 = expressao[2]
+
+    if operador == '+':
+        return int(operando1) + int(operando2)
+    elif operador == '*':
+        return int(operando1) * int(operando2)
+    elif operador == '-':
+        return int(operando1) - int(operando2)
 
 
 @pytest.mark.parametrize('expressao,resultado', [
@@ -14,11 +23,16 @@ def calcule(expressao):
     ("4", 4),
     # um operador
     ('0 + 0', 0),
-    ('0 * 0', 0),
     ('0 + 1', 1),
     ('1 + 1', 0b10),
+    ('1 + 2', 3),
+
+    ('0 * 0', 0),
     ("1 * 1", 1),
     ("1 * 2", 2),
+    ("5 * 2", 10),
+
+    ("5 - 2", 3),
 ])
 def test_calcule(expressao, resultado):
     assert calcule(expressao) == resultado
