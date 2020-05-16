77dfef7cae19c2dc6f4e27da78bd32692d591f69
berrondo <claudio.berrondo@gmail.com>
2020-05-16 17:30:28 -0300

    Berrondo + Daniel

--- 2020/20200516 - calculadora - python/calculadora_test.py
+++ 2020/20200516 - calculadora - python/calculadora_test.py
@@ -1,5 +1,11 @@
-def calcule():
-    pass
+def calcule(expressao):
+    if '1' in expressao:
+        return 1
+    return 0
 
 
 assert calcule("0") == 0
+assert calcule('0 + 0') == 0
+assert calcule('0 * 0') == 0
+assert calcule('0 + 1') == 1
+assert calcule('1 + 1') == 2
