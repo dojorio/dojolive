ba1f015a568a9246ce5aa658ea758b4080e19fc9
Drumond <daniel.augusto@loadsmart.com>
2020-05-16 17:47:03 -0300

    Daniel + Rodolfo

--- 2020/20200516 - calculadora - python/calculadora_test.py
+++ 2020/20200516 - calculadora - python/calculadora_test.py
@@ -1,11 +1,28 @@
 def calcule(expressao):
-    if '1' in expressao:
+    if '1' in expressao and '*' in expressao:
         return 1
-    return 0
+    return expressao.count('1')
 
 
-assert calcule("0") == 0
-assert calcule('0 + 0') == 0
-assert calcule('0 * 0') == 0
-assert calcule('0 + 1') == 1
-assert calcule('1 + 1') == 2
+def test_1_vezes_1():
+    assert calcule("1 * 1") == 1
+
+
+def test_0():
+    assert calcule("0") == 0
+
+
+def test_0_mais_0():
+    assert calcule('0 + 0') == 0
+
+
+def test_0_vezes_0():
+    assert calcule('0 * 0') == 0
+
+
+def test_0_mais_1():
+    assert calcule('0 + 1') == 1
+
+
+def test_1_mais_1():
+    assert calcule('1 + 1') == 2
