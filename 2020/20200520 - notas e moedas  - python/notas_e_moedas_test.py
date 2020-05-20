eb7b27349d81dd9861b5747b85a4347510a39486
Drumond <daniel.augusto@loadsmart.com>
2020-05-20 19:29:43 -0300

    Daniel + Henter

--- 2020/20200520 - notas e moedas  - python/notas_e_moedas_test.py
+++ 2020/20200520 - notas e moedas  - python/notas_e_moedas_test.py
@@ -1,17 +1,30 @@
+import pytest
+
+
 NOTAS = [1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0]
 
 
 def atm(valor):
-    if valor in NOTAS:
-        return {valor: 1}
-    else:
-        return {2.00: 1,
-                1.00: 1}
+    resultado = {}
+    for nota in NOTAS[::-1]:
+        if valor >= nota:
+            resultado[nota] = resultado.get(nota, 0) + 1
+            valor -= nota
+    return resultado
+    # if valor in NOTAS:
+    #     return {valor: 1}
+    # return {2.00: 1, 1.00: 1}
 
 
-def test_atm():
-    assert atm(1.00) == {1.00: 1}
-    assert atm(5.00) == {5.00: 1}
-    assert atm(20.00) == {20.00: 1}
-    assert atm(3.00) == {2.00: 1,
-                         1.00: 1}
+@pytest.mark.parametrize(
+    "valor,notas",
+    (
+        (1.0, {1.0: 1}),
+        (5.00, {5.00: 1}),
+        (20.0, {20.00: 1}),
+        (3.0, {2.00: 1, 1.00: 1}),
+        (4.0, {2.00: 2}),
+    ),
+)
+def test_atm(valor, notas):
+    assert atm(valor) == notas
