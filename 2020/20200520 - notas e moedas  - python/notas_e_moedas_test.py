4febc65e12d072fd154f267d5739ca35d8450c5e
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-05-20 19:04:18 -0300

    Chico + Daniel

--- 2020/20200520 - notas e moedas  - python/notas_e_moedas_test.py
+++ 2020/20200520 - notas e moedas  - python/notas_e_moedas_test.py
@@ -1,5 +1,17 @@
+NOTAS = [1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0]
+
+
 def atm(valor):
-    return {1.00: 1}
+    if valor in NOTAS:
+        return {valor: 1}
+    else:
+        return {2.00: 1,
+                1.00: 1}
+
 
 def test_atm():
     assert atm(1.00) == {1.00: 1}
+    assert atm(5.00) == {5.00: 1}
+    assert atm(20.00) == {20.00: 1}
+    assert atm(3.00) == {2.00: 1,
+                         1.00: 1}
