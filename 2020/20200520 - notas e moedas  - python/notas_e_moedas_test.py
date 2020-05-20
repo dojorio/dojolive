cbe3cd074f1d5e085f945f2847d1a3af0bd04244
Israel Teixeira <israelst@gmail.com>
2020-05-20 20:07:59 -0300

    Israel + Berrondo

--- 2020/20200520 - notas e moedas  - python/notas_e_moedas_test.py
+++ 2020/20200520 - notas e moedas  - python/notas_e_moedas_test.py
@@ -3,33 +3,14 @@ import pytest
 
 NOTAS = [1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0]
 
-# def maior_nota(valor):
-#     for nota in NOTAS[::-1]:
-#         if valor >= nota:
-#             return nota
-#     return
-
 def atm(valor):
-    # if valor == 4:
-    #     return {2.00:2}
-
     resultado = {}
     for nota in NOTAS[::-1]:
-        while valor >= nota:
-            print('nota', nota)
-            print('valor', valor)
-            resultado[nota] = resultado.get(nota, 0) + 1
-            valor -= nota
-
-
-                
-
+        if quantidade_de_notas := valor // nota > 0:
+            resultado[nota] = quantidade_de_notas
+            valor -= resultado[nota] * nota
 
     return resultado
-    # if valor in NOTAS:
-    #     return {valor: 1}
-    # return {2.00: 1, 1.00: 1}
-
 
 @pytest.mark.parametrize(
     "valor,notas",
