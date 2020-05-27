e98fc49dc38df028cd6b0e3cc970da44dfebefbc
rafaelhenter <rafael@henter.com.br>
2020-05-27 19:49:03 -0300

    Rafael + Julia

--- 2020/20200527 - prazos - python/prazos_test.py
+++ 2020/20200527 - prazos - python/prazos_test.py
@@ -6,6 +6,12 @@ from datetime import datetime, timedelta
 def prazo_de_expiracao(data_de_criacao):
     if data_de_criacao.hour < 8:
         data_de_criacao = data_de_criacao.replace(hour=8)
+
+    if data_de_criacao.weekday() == 5:
+        data_de_criacao = data_de_criacao + timedelta(days=2)
+    if data_de_criacao.weekday() == 6:
+        data_de_criacao = data_de_criacao.replace(day=data_de_criacao.day + 1)
+
     if data_de_criacao.hour < 17:
         return data_de_criacao + timedelta(hours=1)
     return datetime(2020, 5, data_de_criacao.day + 1, 9)
@@ -20,6 +26,7 @@ def prazo_de_expiracao(data_de_criacao):
         ((2020, 5, 27, 15), (2020, 5, 27, 16)),
         ((2020, 5, 27, 8), (2020, 5, 27, 9)),
         ((2020, 5, 27, 7), (2020, 5, 27, 9)),
+        ((2020, 5, 30, 7), (2020, 6, 1, 9)),
     ]
 )
 def test_prazo_de_expiracao(data_de_criacao, data_de_expiracao):
