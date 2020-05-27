6f37331d91223bcb2ae6e82cd386402595d7a30c
Drumond <daniel.augusto@loadsmart.com>
2020-05-27 19:25:23 -0300

    Daniel + Chico

--- 2020/20200527 - prazos - python/prazos_test.py
+++ 2020/20200527 - prazos - python/prazos_test.py
@@ -1,6 +1,24 @@
-def prazo_de_expiracao(data_de_criacao):
-    return '2020-05-28 09:00'
+import pytest
 
-def test_prazo_de_expiracao():
-    assert prazo_de_expiracao('2020-05-27 17:00') == '2020-05-28 09:00'
-    assert prazo_de_expiracao('2020-05-26 17:00') == '2020-05-27 09:00'
+from datetime import datetime, timedelta
+
+
+def prazo_de_expiracao(data_de_criacao):
+    if data_de_criacao.hour < 17:
+        return data_de_criacao + timedelta(hours=1)
+    return datetime(2020, 5, data_de_criacao.day + 1, 9)
+
+
+@pytest.mark.parametrize(
+    "data_de_criacao,data_de_expiracao",
+    [
+        ((2020, 5, 27, 17), (2020, 5, 28, 9)),
+        ((2020, 5, 26, 17), (2020, 5, 27, 9)),
+        ((2020, 5, 28, 20), (2020, 5, 29, 9)),
+        ((2020, 5, 27, 15), (2020, 5, 27, 16)),
+        ((2020, 5, 27, 8), (2020, 5, 27, 9)),
+
+    ]
+)
+def test_prazo_de_expiracao(data_de_criacao, data_de_expiracao):
+    assert prazo_de_expiracao(datetime(*data_de_criacao)) == datetime(*data_de_expiracao)
